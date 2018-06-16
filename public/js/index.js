adcaf9ab4ed7f49c7017ce1341577f63e428766f
praful goyal <prafulgoyal2009@gmail.com>
2018-06-16 20:35:29 +0530

    Emit newMessage and createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,6 @@ var socket = io();
 socket.on('connect', function () {
 console.log('connected to server');
 
-socket.emit('createMessage', {
-  from: 'Praful',
-  text: 'Hey. This is praful.'
-});
 });
 
 socket.on('disconnect', function()  {
@@ -15,5 +11,5 @@ console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New Message', message);
+  console.log('newMessage', message);
 });
