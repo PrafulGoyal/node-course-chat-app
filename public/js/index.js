9ea6a970a80b92f432cadd793bb7a627215a2441
praful goyal <prafulgoyal2009@gmail.com>
2018-06-20 01:38:48 +0530

    Add Autoscrollable

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 var socket = io();
 
+function scrollToBottom() {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function () {
 console.log('connected to server');
@@ -19,6 +32,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
   // var li = $('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
   // $('#messages').append(li);
@@ -32,6 +46,7 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
 //   var li = $('<li></li>');
 //   var a = $('<a>My Current Location</a>');
 //   li.text(`${message.from} ${formattedTime}: `);
