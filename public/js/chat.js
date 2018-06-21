f3b6885c17731a467eec713ef977d7b6ceb1bc05
praful goyal <prafulgoyal2009@gmail.com>
2018-06-21 21:45:34 +0530

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -78,7 +78,6 @@ $(document).ready(function () {
   $('#message-form').on('submit', function (e) {
     e.preventDefault();
     socket.emit('createMessage', {
-      from: 'User',
       text: $('[name=message]').val()
     }, function () {
        $('[name=message]').val('');
